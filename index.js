db38252c5ca9b0b909895420d33d75232eb9833c
unknown <sithiroy307@gmail.com>
2024-12-12 15:35:03 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,44 @@
+// // // variable declar and value assign
+// // // data type
+// // // string data typer
+// // const userName = "Sithi"; 
+// // const motherName = "Shila";
+// // const dateOfBirth = "7January"
+// // let userVillage = "Shankarpasa";
+// // let userEmail = "shrabontisikder508@gmail.com";
+// // let season = "Winter";
+// // let time = "2pm";
+
+// // // number data type
+// // const sscPassingYear = 2015 ;
+// // const userNid = 8167178225 ;
+// // let temperature = 28 ;
+
+// // let firstNumber = 300;
+// // let secondNumber = 200;
+// // let thirdNumber = 100;
+// // let fourthNumber = 5;
+// // let sum = firstNumber + secondNumber;
+// // console.log(sum);
+
+// // let substraction = firstNumber - secondNumber;
+// // console.log(substraction);
+
+// // let multiplication = firstNumber * fourthNumber;
+// // console.log(multiplication);
+
+// // let division = secondNumber / fourthNumber;
+// // console.log(division);
+
+// //  let product1Price = 3000;
+// //  let product2Price = 4000;
+// //  let product3Price = 5000;
+
+// // let totalProductPrice = product1Price + product2Price + product3Price;
+// // const discountTaka = totalProductPrice / 10;
+// // console.log(discountTaka);
+
+
+// let age = [ 24, 21, 18, 19, 20];
+// console.log(age);
+
